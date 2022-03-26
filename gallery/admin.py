ed59f18b19de8ba01006b4fced51c1ade548c016
Juliet <agutu@gmail.com>
2022-03-26 05:08:02 +0300

    add image admin class and from models import image

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Editor, Image,
+
 
 # Register your models here.
+
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizantal = ('images',)
+
+admin.site.register(Editor)
+admin.site.register(Image,ImageAdmin)
+
+
+
+
+
