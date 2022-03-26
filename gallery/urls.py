f0327930848215176798eed8995ef8e42751dfee
Juliet <agutu@gmail.com>
2022-03-26 05:02:10 +0300

    import settings and static and media_url

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,12 @@
+# from email.mime import image
+# from unicodedata import category
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import re_path as url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url('',views.imagedisplay)
+    # url(r'^search/', views.search_results, category='search_results')
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
