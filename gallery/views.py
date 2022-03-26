bd11d64df6413ac64b4b7ecb25df68edcea5e2a0
Juliet <agutu@gmail.com>
2022-03-26 05:06:04 +0300

    add function to display images

--- gallery/views.py
+++ gallery/views.py
@@ -1,17 +1,24 @@
 from urllib import request
 from django.shortcuts import render
-from django.http import HttpResponse
-import datetime as dt
+from django.http import HttpResponse, Http404
+from gallery.models import Image
+# from django.template import Template
+
+
 
 # Create your views here.
 
 def welcome(request):
-    return render(request, 'welcome.html')
+    return render (request, 'welcome.html')
+
+
+def imagedisplay(request):  
+    resultsdisplay=Image.objects.all()
+    return render(request,'all-gallery/image.html',{'image':resultsdisplay})  
 
 
 
-# def pic_today(request):
-#     date = dt.date.today()
+
 
 
 
