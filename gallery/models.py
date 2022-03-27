3d8c14265f7b3555b9c2179612afad5b4ff6aadc
Juliet <agutu@gmail.com>
2022-03-27 10:29:43 +0300

    creat class models for image, location and category

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,64 @@
+# from email.mime import image
+from os import name
 from django.db import models
 
+
 # Create your models here.
+
+class Editor(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+
+
+    def __str__(self):
+        return self.first_name
+
+    def save_editor(self):
+        self.save()   
+
+    class Meta:
+        ordering = ['first_name']   
+
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30) 
+
+    def __str__(self):
+        return self.name  
+
+
+class Location(models.Model):
+    location_name = models.CharField(max_length=30) 
+    def __str__(self):
+        return self.location_name       
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='gallery/')
+    description= models.CharField(max_length=200)
+    location = models.ForeignKey ('Location',on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey('Category',on_delete=models.CASCADE, null=True)
+
+    def save_image(self):
+        self.save() 
+
+
+    @classmethod
+    def search_by_category(cls,search_term):
+        image = cls.objects.filter(category_icontains=search_term)
+        return image  
+    
+     
+
+    
+    
+
+
+
+
+
+    
+
