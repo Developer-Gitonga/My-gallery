fb8d3318d2ea48134c70b8814ed78784cb6538c9
Juliet <agutu@gmail.com>
2022-03-27 17:05:33 +0300

    add url patterns for image upload and search

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,7 +6,10 @@ from django.urls import re_path as url
 from . import views
 
 urlpatterns=[
-    url('',views.imagedisplay)
+    url('',views.image),
+    url('',views.image_upload, name = 'image_upload'),
+    url('search/',views.search_results, name='search_results')
     # url(r'^search/', views.search_results, category='search_results')
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
