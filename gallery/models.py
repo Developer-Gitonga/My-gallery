9add1842fb5136100b155b1351fa4597ca4b3d13
Juliet <agutu@gmail.com>
2022-03-27 19:03:32 +0300

    add class method to search for images

--- gallery/models.py
+++ gallery/models.py
@@ -1,5 +1,5 @@
 # from email.mime import image
-from os import name
+# from os import name
 from django.db import models
 
 
@@ -41,15 +41,20 @@ class Image(models.Model):
     description= models.CharField(max_length=200)
     location = models.ForeignKey ('Location',on_delete=models.CASCADE, null=True)
     category = models.ForeignKey('Category',on_delete=models.CASCADE, null=True)
+    search_fields = ['category__name']
+
 
     def save_image(self):
         self.save() 
 
+    def __str__(self):
+        return self.description
+
 
     @classmethod
     def search_by_category(cls,search_term):
-        image = cls.objects.filter(category_icontains=search_term)
-        return image  
+        image = cls.objects.filter(category__name__icontains=search_term)
+        return image
     
      
 
